a41b02fcd6a8302887d37e42eafd63332e3ec9ab
Marc Siegel <marc.siegel@timgroup.com>
2014-04-22 16:33:21 -0400

    Rescue and transform ArgumentError to a Bad by default

--- lib/results.rb
+++ lib/results.rb
@@ -1,4 +1,34 @@
+require 'rescuer'
+
 module Results
+  DEFAULT_EXCEPTIONS_TO_RESCUE_AS_BADS = [ArgumentError]
+  DEFAULT_EXCEPTION_MESSAGE_TRANSFORMS = {
+    ArgumentError => lambda { |m| m.gsub(/\Ainvalid value for [A-Z][a-zA-Z]+\(\):/) { |s| s.gsub('()', '').downcase } }
+  }
+
+  def new
+    raise ArgumentError, 'no block given' unless block_given?
+    exceptions_as_bad = DEFAULT_EXCEPTIONS_TO_RESCUE_AS_BADS
+    exceptions_xforms = DEFAULT_EXCEPTION_MESSAGE_TRANSFORMS
+    from_rescuer(Rescuer.new(*exceptions_as_bad) { yield }, exceptions_xforms)
+  end
+  module_function :new
+
+  def from_rescuer(success_or_failure, exception_message_transforms = DEFAULT_EXCEPTION_MESSAGE_TRANSFORMS)
+    success_or_failure.transform(
+      lambda { |v| Good.new(v) },
+      lambda { |e| Bad.new(transform_exception_message(e, exception_message_transforms)) }
+    ).get
+  end
+  module_function :from_rescuer
+
+  def transform_exception_message(exception, exception_message_transforms = DEFAULT_EXCEPTION_MESSAGE_TRANSFORMS)
+    _, f = exception_message_transforms.find { |klass, _| klass === exception }
+    message = exception && exception.message
+    f && f.call(message) || message
+  end
+  module_function :transform_exception_message
+
   Good = Struct.new(:value)
   Bad  = Struct.new(:error)
 end
--- spec/results_unit_spec.rb
+++ spec/results_unit_spec.rb
@@ -7,6 +7,62 @@ describe Results do
   # Construct indirectly by wrapping a block which may raise an exception
   ##
   describe '.new' do
+
+    context 'when block does *not* raise' do
+      subject { Results.new { 1 } }
+      it { is_expected.to eq Results::Good.new(1) }
+    end
+
+    context 'with defaults' do
+      context 'when block raises ArgumentError' do
+        subject { Results.new { Integer('abc') } }
+        it { is_expected.to eq Results::Bad.new('invalid value for integer: "abc"') }
+      end
+
+      context 'when block raises StandardError' do
+        subject { lambda { Results.new { raise StandardError.new('a message') } } }
+        it { is_expected.to raise_error(StandardError, 'a message') }
+      end
+    end
+
+  end
+
+  ##
+  # Construct directly by wrapping an existing Rescuer::Success or Rescuer::Failure
+  ##
+  describe '.from_rescuer' do
+
+    context 'when success' do
+      let(:success) { Rescuer::Success.new(1) }
+      subject { Results.from_rescuer(success) }
+      it { is_expected.to eq Results::Good.new(1) }
+    end
+
+    context 'when failure' do
+      let(:failure) { Rescuer::Failure.new(StandardError.new('failure message')) }
+      subject { Results.from_rescuer(failure) }
+      it { is_expected.to eq Results::Bad.new('failure message') }
+    end
+
+  end
+
+  ##
+  # Transform exception messages via to configured lambdas
+  ##
+  describe '.transform_exception_message' do
+
+    context 'with defaults' do
+      context 'when argument error due to invalid integer' do
+        subject { Results.transform_exception_message(begin; Integer('abc'); rescue => e; e; end) }
+        it { is_expected.to eq 'invalid value for integer: "abc"' }
+      end
+
+      context 'when argument error due to invalid float' do
+        subject { Results.transform_exception_message(begin; Float('abc'); rescue => e; e; end) }
+        it { is_expected.to eq 'invalid value for float: "abc"' }
+      end
+    end
+
   end
 
   ##
