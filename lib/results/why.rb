75263825832396f9d8f7d4ed03551781dd910c20
Marc Siegel <marc@usainnov.com>
2014-06-15 19:17:02 -0400

    Improve yard documentation of Why()

--- lib/results/why.rb
+++ lib/results/why.rb
@@ -6,13 +6,15 @@ require 'results/why/named'
 module Results
   module_function
 
-  # Wraps `arg` in a `Why` by calling the appropriate constructor.
-  def Why(arg)
-    case arg
-    when Because then Why::One.new(arg)
-    when Array   then Why::Many.new(arg)
-    when Hash    then Why::Named.new(arg)
-    else raise TypeError, "can't convert #{arg.class} into Why"
+  # Wraps `becauses` into the appropriate `Why` depending on its type: `One`, `Many`, or `Named`.
+  # @param [Because, Array<Because>, Hash<Object, Because>] becauses  one or more `Because`'s to wrap in a `Why`
+  # @return [Why::Base]  wrapped explanation(s) as a {Why::One One}, a {Why::Many Many}, or a {Why::Named Named}
+  def Why(becauses)
+    case becauses
+    when Because then Why::One.new(becauses)
+    when Array   then Why::Many.new(becauses)
+    when Hash    then Why::Named.new(becauses)
+    else raise TypeError, "can't convert #{becauses.class} into Why"
     end
   end
 
